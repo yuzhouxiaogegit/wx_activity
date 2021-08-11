f68a18ccb3e81d96a23d35ba6c769d784726a78a
DESKTOP-K2K9NR8\admin-pc <admin@qq.com>
2021-08-11 23:07:56 +0800

    描述更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
 
   data: {
+    show:true,
     storeActivityList: [{
         activity_content: "既然时间是不可逆的，它总是无法被追回。↵↵面对自己匆匆又庸碌的上半年，除去唏嘘抱憾不已，且紧紧抓住下半年这最后的一点时光，去勇敢的做点什么吧。那些你平日渴望又害怕的，叫醒你打盹很久的梦想，弥补这颗粒无收的逝年。↵↵人世间唯一公平的东西，怕就是这捧任性又可人的时间了。在时间面前，没有等级，没有贫贱，没有欺凌。",
         activity_date: "2019-12-09",
@@ -54,7 +55,6 @@ Page({
   },
   // 关闭按钮
   activityCancel(e){
-    console.log("关闭按钮")
   },
   // 查看详情
   activityDetail(e){
